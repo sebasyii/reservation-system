9ac63543b4bb90c399724a7eb687d1c4f71ea10a
sebasyii <sebastian.yii.xh@gmail.com>
2021-02-22 07:23:52 +0800

    Initial Commit

---
+++ src/components/FormBookingDetails.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/core/AppBar';
+import TextField from 'material-ui/core/TextField';
+import RaisedButton from 'material-ui/core/RaisedButton';
+
+export class FormBookingDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Form Booking</h1>
+            </div>
+        )
+    }
+}
+
+export default FormBookingDetails
---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FormUserDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from user Details</h1>
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails
